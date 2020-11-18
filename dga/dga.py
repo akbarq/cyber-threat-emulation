2e91b64bc0055573da8e511546c3ac96df80b9b9
Akbar Qureshi <aq2050@gmail.com>
2020-11-18 17:16:11 -0500

    Update dga.py

--- dga/dga.py
+++ dga/dga.py
@@ -1,1 +1,26 @@
+#############################################################
+# Description: dga.py tries to emulate malware DGA requests #                           
+# Version: 1.0                                              #
+# Author: Akbar Qureshi                                     #
+#############################################################
 
+#!/usr/bin/python3
+import string
+import random
+import requests
+import sys
+
+if len(sys.argv) < 2:
+    print ('Usage: dga.py dga_requests')
+    print ('Example: dga.py 5') 
+    sys.exit(1)
+
+for dga in range(int(sys.argv[1])):
+        dga = (''.join(random.choice(string.ascii_lowercase) for i in range(12)))
+        tlds = ['cc', 'top', 'ws', 'to', 'so', 'gq', 'gdn', 'cf']
+        tld = '{}.{}'.format(dga,random.choice(tlds))
+        print(tld)
+        try:
+            requests.get('http://{}'.format(tld))
+        except:
+                pass
